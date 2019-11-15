1839ca75d17dad066769f698ac5806114804662e
Kason Shiroma <kasons@hawaii.edu>
2019-11-15 10:00:26 -1000

    Created footer

--- app/imports/ui/components/Footer.jsx
+++ app/imports/ui/components/Footer.jsx
@@ -1,19 +1,41 @@
 import React from 'react';
+import { Grid, List, Input, Button, Icon } from 'semantic-ui-react';
 
 /** The Footer appears at the bottom of every page. Rendered by the App Layout component. */
 class Footer extends React.Component {
   render() {
-    const divStyle = { paddingTop: '15px' };
     return (
-        <footer>
-          <div style={divStyle} className="ui center aligned container">
-            <hr />
-              Department of Information and Computer Sciences <br />
-              University of Hawaii<br />
-              Honolulu, HI 96822 <br />
-            <a href="http://ics-software-engineering.github.io/meteor-application-template-react">Template Home Page</a>
-          </div>
-        </footer>
+        <div className="footer-background">
+          <Grid container>
+            <Grid.Column floated='left'>
+              <List>
+                <List.Item>About us</List.Item>
+                <List.Item>Help</List.Item>
+              </List>
+            </Grid.Column>
+
+            <Grid.Column width={4} floated='left'>
+              <List>
+                <List.Item>Sign up for the latest updates</List.Item>
+                <List.Item>
+                  <Input placeholder="Enter email address" />
+                  <Button color="black">Join</Button>
+                </List.Item>
+              </List>
+            </Grid.Column>
+
+            <Grid.Column width={3} floated='right'>
+              <List>
+                <List.Item>Follow us</List.Item>
+                <List.Item>
+                  <Icon padded inverted name="facebook f"/>
+                  <Icon padded inverted name="instagram"/>
+                  <Icon padded inverted name="twitter"/>
+                </List.Item>
+              </List>
+            </Grid.Column>
+          </Grid>
+        </div>
     );
   }
 }
