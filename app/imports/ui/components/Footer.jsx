557e7db91b44aa1cd39c9162a8773c7155cd2362
KWong41 <46613890+KWong41@users.noreply.github.com>
2019-11-18 14:55:51 -1000

    updating landing page

--- app/imports/ui/components/Footer.jsx
+++ app/imports/ui/components/Footer.jsx
@@ -1,19 +1,42 @@
 import React from 'react';
+import { Grid, List, Input, Button, Icon } from 'semantic-ui-react';
 
 /** The Footer appears at the bottom of every page. Rendered by the App Layout component. */
 class Footer extends React.Component {
   render() {
-    const divStyle = { paddingTop: '15px' };
+    const itemPad = { paddingTop: '10px' };
     return (
-        <footer>
-          <div style={divStyle} className="ui center aligned container">
-            <hr />
-              Department of Information and Computer Sciences <br />
-              University of Hawaii<br />
-              Honolulu, HI 96822 <br />
-            <a href="https://johnsonslist.github.io/">JohnsonsList</a>
-          </div>
-        </footer>
+        <div className="footer-background">
+          <Grid container>
+            <Grid.Column width={2}>
+              <List>
+                <List.Item style={itemPad}>About us</List.Item>
+                <List.Item>Help</List.Item>
+              </List>
+            </Grid.Column>
+
+            <Grid.Column width={4}>
+              <List>
+                <List.Item>Sign up for the latest updates</List.Item>
+                <List.Item>
+                  <Input placeholder="Enter email address" />
+                  <Button color="black">Join</Button>
+                </List.Item>
+              </List>
+            </Grid.Column>
+
+            <Grid.Column width={3} floated='right'>
+              <List>
+                <List.Item style={itemPad}>Follow us</List.Item>
+                <List.Item>
+                  <Icon inverted name="facebook f"/>
+                  <Icon inverted name="instagram"/>
+                  <Icon inverted name="twitter"/>
+                </List.Item>
+              </List>
+            </Grid.Column>
+          </Grid>
+        </div>
     );
   }
 }
