f412ceb58f97f3de0d114978034814905b137bb3
Kason Shiroma <kasons@hawaii.edu>
2019-11-19 19:50:45 -1000

    Update Landing.jsx

--- app/imports/ui/pages/Landing.jsx
+++ app/imports/ui/pages/Landing.jsx
@@ -4,16 +4,16 @@ import { Grid, Image } from 'semantic-ui-react';
 /** A simple static component to render some text for the landing page. */
 class Landing extends React.Component {
   render() {
+    const gridStyle = { height: '400px' };
     return (
         <div className='landing'>
-          <Grid verticalAlign='middle' textAlign='center' container>
+          <Grid verticalAlign='middle' textAlign='center' container style={gridStyle}>
 
             <Grid.Column width={4}>
               <Image size='medium' circular src="/images/JL-logo2.png"/>
             </Grid.Column>
 
             <Grid.Column width={8} inverted>
-              <h1>JohnsonsList</h1>
               <h3>A sales website dedicated to new incoming and current University of Hawaii students. This proprietary
                 website helps in giving every University of Hawaii college student an opportunity to purchase items with
                 also enabling other students to sell their unneeded stuff.</h3>
