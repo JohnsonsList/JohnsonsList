f934f5064a6c2990ebd55f371c19667e21951759
Jake Camarao <jcamarao@hawaii.edu>
2019-11-21 11:50:01 -1000

    spaced out navbar

--- app/imports/ui/components/NavBar.jsx
+++ app/imports/ui/components/NavBar.jsx
@@ -28,12 +28,12 @@ class NavBar extends React.Component {
             {this.props.currentUser ? (
                 // eslint-disable-next-line react/jsx-key
                 [<Container>
-                  <Menu.Item as={NavLink} activeClassName="active" exact to="/store" key='add'>VIEW STORE</Menu.Item>
-                  <Menu.Item as={NavLink} activeClassName="active" exact to="/yourshop" key='add'>VIEW
+                  <Menu.Item as={NavLink} activeClassName="active" exact to="/store" key='add' position='right'>VIEW STORE</Menu.Item>
+                  <Menu.Item as={NavLink} activeClassName="active" exact to="/yourshop" key='add' position='right'>VIEW
                     MY LISTINGS</Menu.Item>
-                  <Menu.Item as={NavLink} activeClassName="active" exact to="/add" key='list'>SELL AN ITEM</Menu.Item>
-                  <Menu.Item as={NavLink} activeClassName="active" exact to="/list" key='add'>YOUR SHOP</Menu.Item>
-                  <Menu.Item as={NavLink} activeclassNAme="active" exact to="/saved" key="add">SAVED ITEMS
+                  <Menu.Item as={NavLink} activeClassName="active" exact to="/add" key='list' position='right'>SELL AN ITEM</Menu.Item>
+                  <Menu.Item as={NavLink} activeClassName="active" exact to="/list" key='add' position='right'>YOUR SHOP</Menu.Item>
+                  <Menu.Item as={NavLink} activeclassNAme="active" exact to="/saved" key="add" position='right'>SAVED ITEMS
                     <Icon name='star'></Icon></Menu.Item>
                 </Container>]
             ) : ''}
