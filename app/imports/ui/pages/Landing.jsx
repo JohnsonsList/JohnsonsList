6053b8a2b10e01e612e2f2ea2c78a12266bbbdf8
Jake Camarao <jcamarao@hawaii.edu>
2019-11-28 21:57:59 -1000

    she pretty now

--- app/imports/ui/pages/Landing.jsx
+++ app/imports/ui/pages/Landing.jsx
@@ -21,9 +21,9 @@ class Landing extends React.Component {
           <Grid verticalAlign='middle' textAlign='left' container style={gridStyle}>
             <Grid.Column width={6}>
               <h2>A convenient website to buy and sell goods for UH campus life</h2>
-              <h3 color={ 'white' } >Sign up to browse and shop with other students today</h3>
+              <h3 color={ 'white' } >Sign up to browse and shop with other students right now</h3>
               <Button color='green' as={NavLink} exact to='/signup'>
-                <Icon name='envelope'/>Sign up with email</Button>
+                <Icon name='envelope'/>Sign up with UH email</Button>
             </Grid.Column>
             <Grid.Column width={8}>
             </Grid.Column>
@@ -45,14 +45,18 @@ class Landing extends React.Component {
             </Menu>
              <Segment id='test' basic>Why is this like this?</Segment>
               <h1 align='center'>WHY JOHNSONSLIST WORKS</h1>
-            <divider/>
-              <Grid.Column textAlign='center' width={3}>
-                <h4><Icon size='huge' name='registered'/>
-                Only verified UH students can sign up and use the website</h4>
+            <Grid>
+              <Grid.Column textAlign='left' width={4}>
+                <Icon.Group size='huge'>
+                <Icon color='grey' name='registered'/>
+                <Icon color='grey' corner name='check'/>
+                </Icon.Group>
+                <h4>Only verified UH students can sign up and use the website</h4>
               </Grid.Column>
-              <Grid.Column>
-                <h4></h4>
+              <Grid.Column width={3}>
+                <h4>Test yeah idk why it works</h4>
               </Grid.Column>
+            </Grid>
             </Container>
           </Grid.Column>
             </Container>
