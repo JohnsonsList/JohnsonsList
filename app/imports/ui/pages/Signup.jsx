f3172170d24d5e2d7667529d39c2754d0355aae8
Jake Camarao <jcamarao@hawaii.edu>
2019-11-29 21:09:52 -1000

    this is broken af lmao

--- app/imports/ui/pages/Signup.jsx
+++ app/imports/ui/pages/Signup.jsx
@@ -1,9 +1,9 @@
 import React from 'react';
 import PropTypes from 'prop-types';
 import { Link, Redirect } from 'react-router-dom';
-import { Container, Form, Grid, Header, Message, Segment } from 'semantic-ui-react';
+import { Container, Form, Grid, Message, Segment } from 'semantic-ui-react';
 import { Accounts } from 'meteor/accounts-base';
-import SOBar from '../components/SOBar';
+import SUBar from '../components/SUBar';
 
 /**
  * Signup component is similar to signin component, but we create a new user instead.
@@ -12,7 +12,7 @@ class Signup extends React.Component {
   /** Initialize state fields. */
   constructor(props) {
     super(props);
-    this.state = { email: '', password: '', error: '', redirectToReferer: false };
+    this.state = { user: '', email: '', password: '', error: '', redirectToReferer: false };
   }
 
   /** Update the form controls each time the user interacts with them. */
@@ -22,8 +22,8 @@ class Signup extends React.Component {
 
   /** Handle Signup submission. Create user account and a profile entry, then redirect to the home page. */
   submit = () => {
-    const { email, password } = this.state;
-    Accounts.createUser({ email, username: email, password }, (err) => {
+    const { user, email, password } = this.state;
+    Accounts.createUser({ user, email, username: email, password }, (err) => {
       if (err) {
         this.setState({ error: err.reason });
       } else {
@@ -41,25 +41,30 @@ class Signup extends React.Component {
     }
     return (
         <div>
-          <SOBar/>
+          <SUBar/>
+          <div className='sign-page'>
+          <div id='sign-up'>
       <Container>
         <Grid textAlign="center" verticalAlign="middle" centered columns={2}>
           <Grid.Column>
-            <Header as="h2" textAlign="center">
-              Register your account
-            </Header>
             <Form onSubmit={this.submit}>
+              <h2 centered>
+                Register your account
+              </h2>
               <Segment stacked>
-                <Form.Input
-                  label="Email"
-                  icon="user"
-                  iconPosition="left"
-                  name="email"
-                  type="email"
+                <Form.Group>
+                  <Form.Input
+                      width={3} label='Username' name='user'
+                      placeholder='First Name' type='user'
+                      onChange={this.handleChange()}
+                  />
+                <Form.Input width={10}
+                  label="Email" icon="user" iconPosition="left" name="email" type="email"
                   placeholder="E-mail address"
                   onChange={this.handleChange}
                 />
-                <Form.Input
+                </Form.Group>
+                <Form.Input width={10}
                   label="Password"
                   icon="lock"
                   iconPosition="left"
@@ -86,6 +91,8 @@ class Signup extends React.Component {
           </Grid.Column>
         </Grid>
       </Container>
+          </div>
+          </div>
         </div>
     );
   }
