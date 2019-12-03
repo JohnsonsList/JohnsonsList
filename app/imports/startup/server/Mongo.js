5cc387e5382d0f674a864172e086cc7040862262
Jake Camarao <jcamarao@hawaii.edu>
2019-12-03 09:19:03 -1000

    got it to work lol

--- app/imports/startup/server/Mongo.js
+++ app/imports/startup/server/Mongo.js
@@ -1,5 +1,6 @@
 import { Meteor } from 'meteor/meteor';
 import { Stuffs } from '../../api/stuff/Stuff.js';
+import { Issues } from '../../api/issue/Issue.js';
 
 /* eslint-disable no-console */
 
@@ -14,5 +15,19 @@ if (Stuffs.find().count() === 0) {
   if (Meteor.settings.defaultData) {
     console.log('Creating default data.');
     Meteor.settings.defaultData.map(data => addData(data));
+  }
+}
+
+/** Initialize the database with a default data document. */
+function addData2(data) {
+  console.log(`  Adding: ${data.name} (${data.owner})`);
+  Issues.insert(data);
+}
+
+/** Initialize the collection if empty. */
+if (Issues.find().count() === 0) {
+  if (Meteor.settings.defaultIssue) {
+    console.log('Creating default data.');
+    Meteor.settings.defaultIssue.map(data => addData2(data));
   }
 }
