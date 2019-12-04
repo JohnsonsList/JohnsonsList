0d22d53070de31ce7cd4ae335f70bb93795ee747
Kason Shiroma <kasons@hawaii.edu>
2019-12-03 23:12:18 -1000

    Revert "everything to do with categories is broken..."
    This reverts commit 531b2f8bff63bee0bcb51fe5dc76a34151c7a570.

--- app/imports/api/stuff/Stuff.js
+++ app/imports/api/stuff/Stuff.js
@@ -2,11 +2,8 @@ import { Mongo } from 'meteor/mongo';
 import SimpleSchema from 'simpl-schema';
 import { Tracker } from 'meteor/tracker';
 
-/** The name of the collection and the global publication. */
-const stuffsName = 'Stuffs';
-
 /** Define a Mongo collection to hold the data. */
-const Stuffs = new Mongo.Collection('stuffsName');
+const Stuffs = new Mongo.Collection('Stuffs');
 
 /** Define a schema to specify the structure of each document in the collection. */
 const StuffSchema = new SimpleSchema({
@@ -26,4 +23,4 @@ const StuffSchema = new SimpleSchema({
 Stuffs.attachSchema(StuffSchema);
 
 /** Make the collection and schema available to other code. */
-export { Stuffs, StuffSchema, stuffsName };
+export { Stuffs, StuffSchema };
