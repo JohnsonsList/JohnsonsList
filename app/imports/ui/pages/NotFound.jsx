0527d65407d28fefd6736c32f616e8fffcd3691a
Kason Shiroma <kasons@hawaii.edu>
2019-12-09 23:30:45 -1000

    fixing some ugly fonts

--- app/imports/ui/pages/NotFound.jsx
+++ app/imports/ui/pages/NotFound.jsx
@@ -5,14 +5,15 @@ import { Header, Loader } from 'semantic-ui-react';
 class NotFound extends React.Component {
   render() {
     const pageStyle = { paddingTop: '20px', paddingBottom: '50px' };
+    const fontStyle = { fontFamily: 'lato !important' };
     return (
         <div style={pageStyle}>
-          <Header as="h2" textAlign="center">
-          <p>Uh oh! This seems like a faulty link.</p>
-          <p>Redirecting you to the home page...</p>
-        </Header>
-        <Loader large active inline='centered' />
-        <meta httpEquiv="refresh" content = "5; url=/#/" />
+          <Header as="h2" textAlign="center" style={fontStyle}>
+            <p>Uh oh! This seems like a faulty link.</p>
+            <p>Redirecting you to the home page...</p>
+          </Header>
+          <Loader large active inline='centered'/>
+          <meta httpEquiv="refresh" content="5; url=/#/"/>
         </div>
     );
   }
